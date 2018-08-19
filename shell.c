0720790aee280142ffff2a01412530cd05242a65
2xsec <dh48.jeong@samsung.com>
2018-08-19 15:40:53 +0900

    add some comments for building 32 bit binary
    Signed-off-by: 2xsec <dh48.jeong@samsung.com>

--- shell.c
+++ shell.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 
-// 64 bit machine
-// gcc -o shell shell.c -fno-stack-protector -mpreferred-stack-boundary=4 -static
-
+/*
+ * build machine - 64 bit
+ * In order to build 32 bit binary on 64 bit machine, -m32 and -mpreferred-stack-boundary=2 options are used
+ * -m32 / -m64 is needed to install gcc-multilib package
+ *
+ * gcc -o shell shell.c -m32 -fno-stack-protector -mpreferred-stack-boundary=2 -static
+ */
 int main() {
-	char *name[2] = {"sh", NULL};
-	execve("/bin/sh", name, NULL);
+	char *name[2] = {"/bin/sh", NULL};
+	execve(name[0], name, NULL);
 	return 0;
 }
